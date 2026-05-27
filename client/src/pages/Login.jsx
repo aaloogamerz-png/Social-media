c19f0548b95443703eac307667b2f5e14f688fb2
aaloogamerz-png <aaloogamerz@gmail.com>
2026-05-27 21:54:54 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,1 +1,112 @@
+import { useState } from "react";
 
+import axios from "axios";
+
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+
+const navigate = useNavigate();
+
+const [isRegister, setIsRegister] =
+useState(false);
+
+const [formData, setFormData] =
+useState({
+username: "",
+email: "",
+password: "",
+});
+
+const handleSubmit = async () => {
+
+try {
+
+const url = isRegister
+? "http://localhost:5000/api/auth/register"
+: "http://localhost:5000/api/auth/login";
+
+const res = await axios.post(
+url,
+formData
+);
+
+if(!isRegister) {
+
+localStorage.setItem(
+"token",
+res.data.token
+);
+
+navigate("/home");
+
+}
+
+} catch(err) {
+
+console.log(err);
+
+}
+
+};
+
+return (
+
+<div>
+
+<h1>Social Media App</h1>
+
+{
+isRegister && (
+
+<input
+placeholder="Username"
+onChange={(e) =>
+setFormData({
+...formData,
+username: e.target.value,
+})
+}
+/>
+
+)
+}
+
+<input
+placeholder="Email"
+onChange={(e) =>
+setFormData({
+...formData,
+email: e.target.value,
+})
+}
+/>
+
+<input
+type="password"
+placeholder="Password"
+onChange={(e) =>
+setFormData({
+...formData,
+password: e.target.value,
+})
+}
+/>
+
+<button onClick={handleSubmit}>
+{isRegister ? "Register" : "Login"}
+</button>
+
+<button
+onClick={() =>
+setIsRegister(!isRegister)
+}
+>
+Switch
+</button>
+
+</div>
+
+);
+
+}
