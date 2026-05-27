e75673c2136b183456ff395ff20472650f606693
aaloogamerz-png <aaloogamerz@gmail.com>
2026-05-27 21:37:09 +0530

    Add User model with schema for MongoDB

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,30 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema(
+{
+  username: String,
+  email: String,
+  password: String,
+  profilePic: String,
+
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+},
+{ timestamps: true }
+);
+
+module.exports = mongoose.model(
+"User",
+UserSchema
+);
