d284e84b7ae4d11676a2ef0e89f6dc569bdad22a
aaloogamerz-png <aaloogamerz@gmail.com>
2026-05-27 21:31:56 +0530

    Initialize server with Express, MongoDB, and Socket.io
    Set up Express server with MongoDB and Socket.io

--- server/server.js
+++ server/server.js
@@ -1,1 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/posts");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected");
+
+  socket.on("sendMessage", (data) => {
+    io.emit("receiveMessage", data);
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("Server Running");
+});
