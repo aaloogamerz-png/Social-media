a14cd35d17cff66ea9a2f93925c340fd0242a023
aaloogamerz-png <aaloogamerz@gmail.com>
2026-05-27 21:38:22 +0530

    Update Post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,1 +1,29 @@
+const mongoose = require("mongoose");
 
+const PostSchema = new mongoose.Schema(
+{
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  caption: String,
+
+  mediaUrl: String,
+
+  mediaType: String,
+
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+},
+{ timestamps: true }
+);
+
+module.exports = mongoose.model(
+"Post",
+PostSchema
+);
